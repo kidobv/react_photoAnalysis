3af3fb50c2b198cfe64475079553f4c100072f80
Kidany <kidobv@gmail.com>
2019-08-09 18:10:30 -0400

    Updates

---
+++ precache-manifest.3dc845aae65a03e468c7f82a1e46d30b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b31c611c9429bb8f4c23c0bef6d85aea",
+    "url": "/react_photoAnalysis/index.html"
+  },
+  {
+    "revision": "4b341f96984d58e8208d",
+    "url": "/react_photoAnalysis/static/css/main.3fb0fc49.chunk.css"
+  },
+  {
+    "revision": "2be7516bf1eaa535950c",
+    "url": "/react_photoAnalysis/static/js/2.b2c8fd87.chunk.js"
+  },
+  {
+    "revision": "4b341f96984d58e8208d",
+    "url": "/react_photoAnalysis/static/js/main.01ca3e2d.chunk.js"
+  },
+  {
+    "revision": "a28ed9698be5022cf8ca",
+    "url": "/react_photoAnalysis/static/js/runtime~main.48bf1095.js"
+  }
+]);
